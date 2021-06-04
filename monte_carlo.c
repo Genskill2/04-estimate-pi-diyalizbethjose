359bf6c26dac57116d92db0ecc5e4eddb45aca5a
diyalizbethjose <84697287+diyalizbethjose@users.noreply.github.com>
2021-06-04 09:13:20 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -41,9 +41,8 @@ int main(void) {
 float mc_pi(int n)
 {
   int count=0;
-  int i;
-  double x,y,z,pi,p;
-  for(i=0;i<n;i++)
+  float x,y,z,pi,p;
+  for(int i=0;i<n;i++)
   {
     x= frandom();
     y=frandom();
@@ -54,7 +53,7 @@ float mc_pi(int n)
         count++;
       }
   }
-    pi= (double)(count/n)*4;
+    pi= (float)(count/n)*4;
   return(pi);
   
 }
